121501e5c6a0271a603930f3bada637cb8ef56a4
b.gashi <gashi-blenda@hotmail.com>
2025-07-13 14:59:47 +0200

    Fix a bug where UPC is not being saved in the database

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
@@ -36,11 +36,12 @@ import java.util.UUID;
 public class BeerOrderLine extends BaseEntity {
 
     @Builder
-    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
+    public BeerOrderLine(UUID id, String upc, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
                          BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
                          Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
+        this.upc = upc;
         this.beerId = beerId;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
@@ -48,7 +49,7 @@ public class BeerOrderLine extends BaseEntity {
 
     @ManyToOne
     private BeerOrder beerOrder;
-
+    private String upc;
     private UUID beerId;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
